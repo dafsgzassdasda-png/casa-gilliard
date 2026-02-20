db4826c8d0c9b965f0a123a410dee20578d7c235
dafsgzassdasda-png <dafsgzassdasda@gmail.com>
2026-02-20 17:36:56 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,329 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tv",
+      "name": "tv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 1.8873369317767406,
+        "pitch": 0.33656007763458895,
+        "fov": 1.366376471345483
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.380042272197077,
+          "pitch": 0.0962206806933974,
+          "rotation": 1.5707963267948966,
+          "target": "4-entrada"
+        },
+        {
+          "yaw": -0.02882299495357543,
+          "pitch": 0.44850187153122967,
+          "rotation": 0,
+          "target": "5-quarto-tio"
+        },
+        {
+          "yaw": 1.3858290664230033,
+          "pitch": 0.33722541575153997,
+          "rotation": 0,
+          "target": "1-cuzinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cuzinha",
+      "name": "cuzinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.068326409274615,
+          "pitch": 0.38151007179321184,
+          "rotation": 0,
+          "target": "4-entrada"
+        },
+        {
+          "yaw": -2.273701316520949,
+          "pitch": 0.6948679154994863,
+          "rotation": 0,
+          "target": "0-tv"
+        },
+        {
+          "yaw": 1.5649771997420352,
+          "pitch": 0.4145355075725057,
+          "rotation": 0,
+          "target": "2-fugao"
+        },
+        {
+          "yaw": -0.0077956166465575905,
+          "pitch": 0.4681261811743376,
+          "rotation": 0,
+          "target": "6-gourmet"
+        },
+        {
+          "yaw": 1.5573983177547195,
+          "pitch": 0.8326653706331832,
+          "rotation": 0,
+          "target": "3-sala-de-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-fugao",
+      "name": "fugao",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3699112159035227,
+          "pitch": 0.36664263120305307,
+          "rotation": 6.283185307179586,
+          "target": "0-tv"
+        },
+        {
+          "yaw": -1.0413807126607821,
+          "pitch": 0.6272330485438253,
+          "rotation": 0,
+          "target": "1-cuzinha"
+        },
+        {
+          "yaw": -1.6186321173997698,
+          "pitch": 0.591494312473781,
+          "rotation": 0,
+          "target": "3-sala-de-cozinha"
+        },
+        {
+          "yaw": -0.21367666592300516,
+          "pitch": 0.23540471878382618,
+          "rotation": 0,
+          "target": "6-gourmet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sala-de-cozinha",
+      "name": "sala de cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3176091528953258,
+          "pitch": 0.3960145064706069,
+          "rotation": 0,
+          "target": "2-fugao"
+        },
+        {
+          "yaw": 1.6707664152461277,
+          "pitch": 0.6095898396100772,
+          "rotation": 0,
+          "target": "1-cuzinha"
+        },
+        {
+          "yaw": 1.085876733126213,
+          "pitch": 0.5690448444547851,
+          "rotation": 5.497787143782138,
+          "target": "0-tv"
+        },
+        {
+          "yaw": -2.9277888917647275,
+          "pitch": 0.9270262592042045,
+          "rotation": 6.283185307179586,
+          "target": "6-gourmet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-entrada",
+      "name": "entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2976104813511835,
+          "pitch": 0.8387134053641496,
+          "rotation": 11.780972450961727,
+          "target": "0-tv"
+        },
+        {
+          "yaw": -2.7321596452391326,
+          "pitch": 0.9040869941528324,
+          "rotation": 0,
+          "target": "1-cuzinha"
+        },
+        {
+          "yaw": 2.79743648314985,
+          "pitch": 0.9180228694479009,
+          "rotation": 6.283185307179586,
+          "target": "5-quarto-tio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-quarto-tio",
+      "name": "quarto tio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.156410495241289,
+          "pitch": 0.8279300705510657,
+          "rotation": 0,
+          "target": "0-tv"
+        },
+        {
+          "yaw": 0.5874225551191934,
+          "pitch": 0.6885876754820615,
+          "rotation": 5.497787143782138,
+          "target": "6-gourmet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-gourmet",
+      "name": "gourmet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11613658563289064,
+          "pitch": 0.7947808929080757,
+          "rotation": 0,
+          "target": "1-cuzinha"
+        },
+        {
+          "yaw": -0.7444744250724575,
+          "pitch": 0.5980517198083035,
+          "rotation": 5.497787143782138,
+          "target": "2-fugao"
+        },
+        {
+          "yaw": -0.36697401235945293,
+          "pitch": 0.5516200227637,
+          "rotation": 0,
+          "target": "3-sala-de-cozinha"
+        },
+        {
+          "yaw": 2.9134073874793547,
+          "pitch": 0.9075494123254231,
+          "rotation": 5.497787143782138,
+          "target": "5-quarto-tio"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casa do Gilliard",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
